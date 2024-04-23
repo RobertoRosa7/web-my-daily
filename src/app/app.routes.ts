9bcd4ae84a087f6047d4bf9a9fa513f42f2efa7e
VictorSalome <victorsalome41@hotmail.com>
2024-04-23 14:11:52 -0300

    feat: create component register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 export const routes: Routes = [
-    
+  {
+    path: 'auth/register',
+    component: RegisterComponent,
+  },
 ];
