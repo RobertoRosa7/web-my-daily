7d414585dc3943bfaf60637578147da76e41938e
Roberto Rosa <roberto.rosa7@gmail.com>
2024-12-03 06:49:09 -0300

    fix: env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  host: 'https://api.innovatenet.local/api', // gateway
-  ws: 'wss://api.innovatenet.local/ws', // ms-daily
+  host: 'https://api.innovatenet.local', // gateway
+  ws: 'ws://api.innovatenet.local/ws', // ms-daily
 };
