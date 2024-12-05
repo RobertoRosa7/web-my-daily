498f636e2d212f99b8f94fc65ee0af406098a3a5
Roberto Rosa <roberto.rosa7@gmail.com>
2024-12-04 22:38:24 -0300

    fix: broken file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -44,6 +44,6 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })
+    }), provideAnimationsAsync()
 ],
 };
