58ec7fe77efeeefe03a140e0ca74c328f2058f44
Roberto Rosa <roberto.rosa7@gmail.com>
2024-12-05 06:56:00 -0300

    remove ssl

--- server.ts
+++ server.ts
@@ -31,6 +31,7 @@ export function app(): express.Express {
   server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
+    process.stdout.write(`Angular SSR: ${req.url}\n`);
     commonEngine
       .render({
         bootstrap,
